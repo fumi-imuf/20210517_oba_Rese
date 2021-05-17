19e7c79eaf507b2f0b908809e0127cdeb466d683
fumi <mo2min_00823@yahoo.co.jp>
2021-05-17 18:52:26 +0900

    ReservationController-edit

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ReservationsController extends Controller
 {
-    //
+    public function post(Request $request) {
+        $now = Carbon::now();
+        $param = [
+            "user_id" => $request->user_id,
+            "shop_id" => $request->shop_id,
+            "reservation_date" => $request->reservation_date,
+            "reservation_time" => $request->reservation_time,
+            "reservation_number" => $request->reservation_number,
+            "created_at" => $now,
+            "updated_at" => $now
+        ];
+        DB::table('reservations')->insert($param);
+        return response()->json([
+            'message' => 'Reservation created successfully',
+            'data' => $param
+        ], 200);
+    }
 }
