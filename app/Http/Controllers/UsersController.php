5a1f6d52bb55246578681391edf6e89b42803163
fumi <mo2min_00823@yahoo.co.jp>
2021-06-15 16:51:05 +0900

    usersController test

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\DB;
 class UsersController extends Controller
 {
     public function get(Request $request) {
-    if ($request->has('email')) {
-        $items = DB::table('users')->where('email', $request->email)->get();
+    if ($request->has("email")) {
+        $items = DB::table("users")->where("email", $request->email)->get();
         return response()->json([
             'message' => 'User got successfully',
             'data' => $items
